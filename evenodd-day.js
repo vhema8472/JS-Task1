7c96cffa2a5925c325a9cef846cbcae7efd231dd
vhema8472 <vhema8472@gmail.com>
2025-09-24 18:50:08 +0530

    first commit

---
+++ evenodd-day.js
@@ -0,0 +1,32 @@
+// Input as string 
+let x="12";
+
+// Convert a number
+let num=Number(x);
+
+
+// Even or odd function 
+
+let Num=num%2==0 ? "Even" : "Odd" ;
+console.log(Num);
+
+
+// input as string
+let y = "4";
+ 
+// convert to number
+let day=Number(y);
+
+// uses switch case
+switch(true){
+    case(1<=day && 5>=day):
+      date="It's a weekday";
+      break;
+    case(6<=day && 7>=day):
+     date="It's the weekend";
+     break;
+    default:
+        date="Invalid day";
+}
+
+console.log(date);
---
+++ grade-calculator.js
@@ -0,0 +1,38 @@
+
+//  Step-1 : input as string
+
+let a = "85";
+
+
+ // Step-2 : type conversion 
+
+let mark1 = Number(a);
+
+
+// Uses  switch statements :
+
+let grade ;
+switch(true){
+    case(mark1>=90 && mark1<=100):
+        grade='A';
+        break;
+    case(75<=mark1 && 89>=mark1):
+        grade='B';
+        break;
+
+    case(50<=mark1 && 74>=mark1):
+        grade='C';
+        break;
+
+    case(50>mark1):
+        grade="Fail";
+        break;
+
+    default:
+        grade="Invalid mark";
+}
+
+
+// Prints the grade 
+
+console.log(grade);   
